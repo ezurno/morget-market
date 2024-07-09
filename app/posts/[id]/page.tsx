81a8c7ac8544576b666e52f68581a9a5f4ff4217
Lee-Junmo <ezurno@gmail.com>
2024-07-09 15:31:37 +0900

    Feat: cache-tag 를 사용해 실시간 데이터 사용
    1. 하지만 해당 방법으로 설계를 하면 사용감 개선이 전혀 되지 않음
    서버에서 실행하는 동안 사용자는 요청이 된 건지 요청이 진행 중인 건지 알 수가 없음
    
    2. 따라서 useOptimistic() 을 활용 할 예정

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -2,9 +2,24 @@ import db from "@/lib/db";
 import { notFound } from "next/navigation";
 import Image from "next/image";
 import { formatToTimeAgo } from "@/lib/utils";
-import { EyeIcon } from "@heroicons/react/24/solid";
-import { HandThumbUpIcon } from "@heroicons/react/24/solid";
+import { EyeIcon, HandThumbUpIcon } from "@heroicons/react/24/solid";
+import { HandThumbUpIcon as OutlineHandThumbUpIcon } from "@heroicons/react/24/outline";
 import { getSession } from "@/lib/sessions/session";
+import { unstable_cache as nextCache, revalidateTag } from "next/cache";
+
+const getCachedPost = nextCache(getPost, ["post-detail"], {
+  tags: ["post-detail"],
+});
+async function getCachedLikeStatus(postId: number, userId: number) {
+  const cachedOperation = nextCache(
+    (postId) => getLikeStatus(postId, userId),
+    ["product-like-status"],
+    {
+      tags: [`like-status-${postId}`],
+    }
+  );
+  return cachedOperation(postId);
+}
 
 async function getPost(id: number) {
   try {
@@ -12,6 +27,8 @@ async function getPost(id: number) {
       where: {
         id,
       },
+      // atomic 으로 현재 값을 몰라도 값을 변경할 수 있음
+      // 현재 해당하는 views 의 값을 1 증가 시키는 상황
       data: {
         views: {
           increment: 1,
@@ -24,10 +41,10 @@ async function getPost(id: number) {
             avatar: true,
           },
         },
+        // _count 로 하위의 갯수를 파악 가능
         _count: {
           select: {
             comments: true,
-            likes: true,
           },
         },
       },
@@ -39,18 +56,24 @@ async function getPost(id: number) {
   }
 }
 
-async function getIsLiked(postId: number) {
-  const session = await getSession();
-  const like = await db.like.findUnique({
+async function getLikeStatus(postId: number, userId: number) {
+  const isLiked = await db.like.findUnique({
     where: {
       id: {
         postId,
-        userId: session.id!,
+        userId,
       },
     },
   });
-
-  return Boolean(like);
+  const likeCount = await db.like.count({
+    where: {
+      postId,
+    },
+  });
+  return {
+    likeCount,
+    isLiked: Boolean(isLiked),
+  };
 }
 
 export default async function PostDetail({
@@ -60,10 +83,12 @@ export default async function PostDetail({
 }) {
   const id = Number(params.id);
   if (isNaN(id)) return notFound();
-  const post = await getPost(id);
+  const post = await getCachedPost(id);
   if (!post) return notFound();
 
-  const isLiked = await getIsLiked(id);
+  // nextCache 와 getSession 을 함께 쓸 수가 없으므로 찢어서 args 로 넘겨서 사용한다!
+  const session = await getSession();
+  const { likeCount, isLiked } = await getCachedLikeStatus(id, session.id!);
 
   const likePost = async () => {
     "use server";
@@ -75,9 +100,15 @@ export default async function PostDetail({
           userId: session.id!,
         },
       });
+
+      revalidateTag(`like-status-${id}`);
     } catch (error) {}
   };
 
+  /**
+   * 하지만 해당 방법으로 설계를 하면 사용감 개선이 전혀 되지 않음
+   * 서버에서 실행하는 동안 사용자는 요청이 된 건지 요청이 진행 중인 건지 알 수가 없음
+   */
   const dislistPost = async () => {
     "use server";
     try {
@@ -91,6 +122,7 @@ export default async function PostDetail({
           },
         },
       });
+      revalidateTag(`like-status-${id}`);
     } catch (error) {}
   };
   return (
@@ -119,10 +151,22 @@ export default async function PostDetail({
         </div>
         <form action={isLiked ? dislistPost : likePost}>
           <button
-            className={`flex items-center gap-2 text-neutral-400 text-sm border border-neutral-400 rounded-full p-2 hover:bg-neutral-800 transition-colors`}
+            className={`${
+              isLiked
+                ? "bg-orange-500 text-white border-orange-500"
+                : "hover:bg-neutral-800"
+            } flex items-center gap-2 text-neutral-400 text-sm border border-neutral-400 rounded-full p-2 transition-colors`}
           >
-            <HandThumbUpIcon className="size-5" />
-            <span>공감하기 ({post._count.likes})</span>
+            {isLiked ? (
+              <HandThumbUpIcon className="size-5" />
+            ) : (
+              <OutlineHandThumbUpIcon className="size-5" />
+            )}
+            {isLiked ? (
+              <span>{likeCount}</span>
+            ) : (
+              <span>공감하기 ({likeCount})</span>
+            )}
           </button>
         </form>
       </div>
