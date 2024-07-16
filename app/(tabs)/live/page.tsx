c168b3c14de403826aa49d7b23106bf6b3a03ada
Lee-Junmo <ezurno@gmail.com>
2024-07-16 15:00:02 +0900

    Feat: live-streaming 구현

--- app/(tabs)/live/page.tsx
+++ app/(tabs)/live/page.tsx
@@ -1,3 +1,17 @@
-export default function Live() {
-  return <h2>LIVE</h2>;
+import db from "@/lib/db";
+import { PlusIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+
+export default async function Live() {
+  return (
+    <div>
+      <Link
+        href={`/streams/add`}
+        className="bg-orange-500 rounded-full
+      flex items-center justify-center size-16 fixed bottom-24 right-8 text-white transition-colors hover:bg-orange-400"
+      >
+        <PlusIcon className="size-10" />
+      </Link>
+    </div>
+  );
 }
