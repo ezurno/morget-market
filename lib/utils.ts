baf7b6385d9600cedda9fac0f40d03965802ba86
Lee-Junmo <ezurno@gmail.com>
2024-07-18 15:23:11 +0900

    Fix: signup-password & time-formatter

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,12 +5,28 @@ export function formatToWon(price: number) {
 
 // 시간이 얼마나 지났는지 경과여부 계산 로직
 export function formatToTimeAgo(date: string): string {
-  const dayInMs = 1000 * 60 * 60 * 24;
+  const msInSecond = 1000;
+  const msInMinute = msInSecond * 60;
+  const msInHour = msInMinute * 60;
+  const msInDay = msInHour * 24;
+
   const time = new Date(date).getTime();
   const now = new Date().getTime();
-  const diff = Math.round((time - now) / dayInMs);
+  const diffInMs = now - time;
 
-  // 날짜에 맞춰서 형식을 바꿔주는 Intl
   const formatter = new Intl.RelativeTimeFormat("ko");
-  return formatter.format(diff, "days");
+
+  if (diffInMs < msInMinute) {
+    const diffInSeconds = Math.floor(diffInMs / msInSecond);
+    return formatter.format(-diffInSeconds, "seconds");
+  } else if (diffInMs < msInHour) {
+    const diffInMinutes = Math.floor(diffInMs / msInMinute);
+    return formatter.format(-diffInMinutes, "minutes");
+  } else if (diffInMs < msInDay) {
+    const diffInHours = Math.floor(diffInMs / msInHour);
+    return formatter.format(-diffInHours, "hours");
+  } else {
+    const diffInDays = Math.floor(diffInMs / msInDay);
+    return formatter.format(-diffInDays, "days");
+  }
 }
